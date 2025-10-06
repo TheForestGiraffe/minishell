9a3d76ee0cdf0bf2c194afe0ce657a32b2c195b5
Pedro Lima <pedrolima.ep@gmail.com>
2025-10-06 17:35:39 +0200

    #11 WIP: move .h to srcs folder for now

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -6,11 +6,16 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 12:31:40 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/10/06 12:32:10 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/10/06 17:13:51 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHEL_H
+
+
+// Signals (signals.c)
+void    setup_signals(void);
+void    handle_sigint(int signal);
 
 #endif
