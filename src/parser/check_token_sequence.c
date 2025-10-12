a21c91dff9408f19b12c80c958953197c56b13a5
TheForestGiraffe <121735952+TheForestGiraffe@users.noreply.github.com>
2025-10-12 18:55:08 +0200

    Fix norm errors; change return convetion to -1: error; 0: false; 1: true

--- src/parser/check_token_sequence.c
+++ src/parser/check_token_sequence.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_token_sequence.c                                  :+:      :+:    :+:   */
+/*   check_token_sequence.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 13:17:15 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/10/12 15:56:43 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/10/12 18:52:44 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,47 +28,47 @@ int	check_token_sequence(t_token *token_lst)
 	{
 		if (curr_token->type == PIPE)
 		{
-			if (check_pipe(curr_token, token_lst) == -1)
-				return (-1);
+			if (check_pipe(curr_token, token_lst) == 0)
+				return (0);
 		}
 		else if (is_redirection(curr_token->type))
 		{
-			if (check_redirection(curr_token) == -1)
-				return (-1);
+			if (check_redirection(curr_token) == 0)
+				return (0);
 		}
 		curr_token = curr_token->next;
 	}
-	return (0);
+	return (1);
 }
 
-static int check_pipe(t_token *curr_token, t_token *token_lst) 
+static int	check_pipe(t_token *curr_token, t_token *token_lst)
 {
 	if ((curr_token == token_lst) && (curr_token->type == PIPE))
 	{
 		ft_putstr_fd("minishell: syntax error near token '|'\n", 2);
-		return (-1);
+		return (0);
 	}
 	if ((curr_token->type == PIPE) && (curr_token->next))
 	{
 		if (curr_token->next->type == PIPE)
 		{
 			ft_putstr_fd("minishel: syntax error near token '|'\n", 2);
-			return (-1);
+			return (0);
 		}
 	}
 	if ((curr_token->type == PIPE) && (!curr_token->next))
 	{
 		ft_putstr_fd("minishel: syntax error near token '|'\n", 2);
-		return (-1);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
 
 static int	is_redirection(t_token_type type)
 {
 	if (type == INPUT || type == OUTPUT || type == RINPUT || type == ROUTPUT)
-		return (true);
-	return (false);
+		return (1);
+	return (0);
 }
 
 static int	check_redirection(t_token *token)
@@ -77,14 +77,14 @@ static int	check_redirection(t_token *token)
 	{
 		ft_putstr_fd("minishel: syntax error near token '", 2);
 		ft_putstr_fd(token->content, 2);
-		ft_putstr_fd("'\n", 2); 
-		return (-1);
-	}
-	if ((token->next->type == WORD) || (token->next->type == S_QT) ||
-		(token->next->type == D_QT))
+		ft_putstr_fd("'\n", 2);
 		return (0);
+	}
+	if ((token->next->type == WORD) || (token->next->type == S_QT)
+		|| (token->next->type == D_QT))
+		return (1);
 	ft_putstr_fd("minishel: syntax error near token '", 2);
 	ft_putstr_fd(token->content, 2);
-	ft_putstr_fd("'\n", 2); 
-	return (-1);
+	ft_putstr_fd("\n", 2);
+	return (0);
 }
--- src/parser/parse.c
+++ src/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 13:34:00 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/10/12 17:37:03 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/10/12 18:54:33 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ t_cmd	*parse(char *line)
 	token_lst = tokenizer(line);
 	if (!token_lst)
 		return (NULL);
-	if (check_token_sequence(token_lst) == -1)
+	if ((check_token_sequence(token_lst) == 0)
+		|| check_token_sequence(token_lst) == -1)
 		return (NULL);
 	return (cmd_lst);
 }
