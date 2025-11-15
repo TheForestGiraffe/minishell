8cc3ab249f0fc1c7f79871f0d20369fffaf4eefb
TheForestGiraffe <121735952+TheForestGiraffe@users.noreply.github.com>
2025-11-15 23:48:58 +0100

    Completed: implemented fn_exit

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 19:41:33 by kalhanaw          #+#    #+#             */
-/*   Updated: 2025/11/15 19:11:56 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/11/15 23:40:10 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "types.h"
 
-int is_builtin(t_cmd *cmd_lst);
-int search_builtin_functions(t_exec_context *exec_context);
+int	is_builtin(t_cmd *cmd_lst);
+int	search_builtin_functions(t_exec_context *exec_context);
 
 #endif
--- src/builtin/fn_builtin_exit.c
+++ src/builtin/fn_builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 14:13:04 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/11/15 22:01:48 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/11/15 23:43:30 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,49 @@
 #include "local_builtin.h"
 #include "types.h"
 #include <stdlib.h>
+#include <unistd.h>
 
-static void	handle_exit_with_one_arg(t_exec_context *exec_context)
+static void	handle_exit_with_two_args(t_exec_context *exec_context)
 {
 	int		ret;
+	long	number;
 
-	ret = ft_strtol(exec_context->cmd_lst->argv->next->content,
-			exec_context->exit_state);
+	ret = ft_strtol(exec_context->cmd_lst->argv->next->content, &number);
 	if (ret == -1)
 	{
 		ft_printf("exit: %s: numeric argument required\n",
-			exec_context->cmd_lst->argv->next);
-		exit(2);
+			exec_context->cmd_lst->argv->next->content);
+		exit(255);
 	}
-	ft_printf("exit\n");
+	if (exec_context->main_pid == getpid())
+		ft_printf("exit\n");
+	exec_context->exit_state = (unsigned char)number;
 	exit(exec_context->exit_state);
 }
 
 int	builtin_exit(t_exec_context *exec_context)
 {
 	int		nr_args;
-	t_token	*cur_arg;
+	t_token	*cur_argv;
 
-	if (!exec_context || !exec_context->cmd_lst)
+	if (!exec_context || !exec_context->cmd_lst
+		|| !exec_context->cmd_lst->argv->content)
 		exit(EXIT_FAILURE);
-	nr_args = 0;
-	while (exec_context->cmd_lst->argv->next)
+	cur_argv = exec_context->cmd_lst->argv;
+	nr_args = 1;
+	while (cur_argv->next)
 	{
-		cur_arg = exec_context->cmd_lst->argv->next;
+		cur_argv = cur_argv->next;
 		nr_args++;
 	}
-	if (nr_args == 0 && (exec_context->main_pid == getpid()))
+	if (nr_args == 1)
 	{
-		ft_printf("exit\n");
+		if (exec_context->main_pid == getpid())
+			ft_printf("exit\n");
 		exit(exec_context->exit_state);
 	}
-	if (nr_args == 1)
-		handle_exit_with_one_arg(exec_context);
+	if (nr_args == 2)
+		handle_exit_with_two_args(exec_context);
 	ft_printf("exit: too many arguments\n");
 	exit(1);
 	return (1);
