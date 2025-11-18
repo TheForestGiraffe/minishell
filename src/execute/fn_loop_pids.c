cd6fb24dd17b350fd50312fcf374d97cff4ac258
Pedro Lima <pedrolima.ep@gmail.com>
2025-11-18 01:48:48 +0100

    Fix child memory leak on loop pids - cmd_lst

--- src/execute/fn_loop_pids.c
+++ src/execute/fn_loop_pids.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 16:55:08 by kalhanaw          #+#    #+#             */
-/*   Updated: 2025/11/17 23:13:20 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/11/18 01:46:41 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+
+static void	cleanup_after_child(int **fd_array, int count, int *process_id_arr,
+								t_exec_context *exec_context)
+{
+	clear_fd_array(fd_array, count - 1);
+	free(process_id_arr);
+	cmd_lst_delete_list(&exec_context->cmd_lst);
+}
 
 static int	connect_pipes(int **fd_array, int pos, int count)
 {
@@ -54,21 +62,21 @@ static int	run_on_child(int **fd_array, t_exec_context *exec_context,
 	if (assign_input_output (exec_context->cmd_lst) == -1)
 	{
 		free_envp(exec_context->envp);
-		cmd_lst_delete_list(&(exec_context->cmd_lst));
 		return (-1);
 	}
 	ret = run_cmd(exec_context);
 	free_envp(exec_context->envp);
-	cmd_lst_delete_list(&(exec_context->cmd_lst));
 	return (ret);
 }
 
 int	loop_pids(int *process_id_arr, int **fd_array,
 			int count, t_exec_context *exec_context)
 {
-	int	i;
-	int	ret;
+	int		i;
+	int		ret;
+	t_cmd	*head;
 
+	head = exec_context->cmd_lst;
 	i = 0;
 	while (i < count)
 	{
@@ -78,16 +86,13 @@ int	loop_pids(int *process_id_arr, int **fd_array,
 		if (process_id_arr[i] == 0)
 		{
 			ret = run_on_child(fd_array, exec_context, i, count);
-			clear_fd_array(fd_array, count - 1);
-			free(process_id_arr);
+			exec_context->cmd_lst = head;
+			cleanup_after_child(fd_array, count, process_id_arr, exec_context);
 			exit(ret);
 		}
-		else
-		{
-			if (exec_context->cmd_lst->next)
-				exec_context->cmd_lst = exec_context->cmd_lst->next;
-			i++;
-		}
+		if (exec_context->cmd_lst->next)
+			exec_context->cmd_lst = exec_context->cmd_lst->next;
+		i++;
 	}
 	return (1);
 }
