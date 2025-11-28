7524c40298b8458151f7246af5787a6913654db1
pecavalc <pecavalc@student.42berlin.de>
2025-11-28 15:11:19 +0100

    Free envp and exports if connext_pipes fails.

--- src/execute/fn_loop_pids.c
+++ src/execute/fn_loop_pids.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 16:55:08 by kalhanaw          #+#    #+#             */
-/*   Updated: 2025/11/27 20:39:59 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/11/28 14:52:02 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,12 @@ static int	run_on_child(int **fd_array, t_exec_context *exec_context,
 
 	signal(SIGQUIT, SIG_DFL);
 	if (connect_pipes(fd_array, i, count - 1) == -1)
+	{
+		free_envp(exec_context->envp);
+		free_envp(exec_context->exports);
 		return (-1);
+	}
+
 	if (assign_input_output (exec_context->cmd_lst) == -1)
 	{
 		free_envp(exec_context->envp);
